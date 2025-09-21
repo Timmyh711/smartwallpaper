0812e9d18440a723aba7144d7781c1c2cbf258c8
Timmyh711 <timmy.yunzhehu@gmail.com>
2025-09-21 15:36:10 -0700

    Update sw.js
    original

--- sw.js
+++ sw.js
@@ -1,17 +1,30 @@
-// Change this to your repository name
-var GHPATH = '/smartwallpaper';
- 
-// Choose a different app prefix name
-var APP_PREFIX = 'pwa_';
- 
-// The version of the cache. Every time you change any of the files
-// you need to change this version (version_01, version_02…). 
-// If you don't change the version, the service worker will give your
-// users the old files!
-var VERSION = 'version_02';
- 
-// The files to make available for offline use. make sure to add 
-// others to this list
-var URLS = [    
-  `${GHPATH}/index.html`,
-]
+const CACHE_NAME = "startpage-cache-v1";
+const ASSETS = [
+"index.html",
+"manifest.json",
+"icon-192.png",
+"icon-512.png"
+];
+
+// Install: cache app shell
+self.addEventListener("install", event => {
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+);
+});
+
+// Activate: cleanup old caches
+self.addEventListener("activate", event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+)
+);
+});
+
+// Fetch: serve cached files when offline
+self.addEventListener("fetch", event => {
+event.respondWith(
+caches.match(event.request).then(resp => resp || fetch(event.request))
+);
+});
